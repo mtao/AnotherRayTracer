5c86ade1ae8a8c407a9654c8bdafbfb1d0b9924b
Michael Tao <mtao@dgp.toronto.edu>
2025-07-01 17:25:43 -0400

    removing extras from ray class

--- include/art/Ray.hpp
+++ include/art/Ray.hpp
@@ -11,10 +11,6 @@ struct Ray {
     Point origin;
     Vector3d direction;
 
-    bool hits_bbox(const geometry::Box& bbox, const Rational& min_t) const;
-    bool hits_bbox(const geometry::Box& bbox, const Intersection& isect) const;
-    bool hits_bbox(const geometry::Box& bbox,
-                   const std::optional<Intersection>& isect) const;
     Point operator()(const Rational& t) const;
 };
 std::string format_as(const Ray& r);
